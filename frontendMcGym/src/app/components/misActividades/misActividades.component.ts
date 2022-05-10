3a193e44faaa17b00440af0ad616c5a14d223743
Shadows <dudo@fp.insjoaquimmir.cat>
2022-05-10 13:20:58 +0200

    mis actividades (falta filtrar por id)

--- frontendMcGym/src/app/components/misActividades/misActividades.component.ts
+++ frontendMcGym/src/app/components/misActividades/misActividades.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActividadesService } from 'src/app/services/actividades.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-actividades',
@@ -7,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class misActividadesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public ActividadService: ActividadesService,private cookieService: CookieService, public router : Router) { }
 
   ngOnInit(): void {
+    this.getEmployees();
     console.log("funciona")
+    if (!this.cookieService.get('sesion-token')){
+      this.router.navigate(["/"])
+    }
+  }
+
+  getEmployees(){
+    this.ActividadService.getActividades().subscribe(
+     res => {
+       this.ActividadService.actividades = res;
+
+     },
+     err => console.log(err)
+   )
+ }
+
+  salir(){
+    this.cookieService.delete('sesion-token')
   }
 
 }
