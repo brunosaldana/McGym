386f3c0503c8789fbcf94c52e19ab2e092fcd131
Shadows <dudo@fp.insjoaquimmir.cat>
2022-05-17 00:33:44 +0200

    email solo se envia si existe empleado

--- frontendMcGym/src/app/components/forgot1/forgot1.component.ts
+++ frontendMcGym/src/app/components/forgot1/forgot1.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import {  FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { EmailService } from 'src/app/services/email.service';
 import { CookieService } from 'ngx-cookie-service';
+import { EmplogService } from 'src/app/services/emplog.service';
 
 @Component({
   selector: 'app-forgot1',
@@ -10,11 +11,15 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./forgot1.component.css']
 })
 export class Forgot1Component implements OnInit {
+
+
   title = 'enviarCorreos';
 
   datos!: FormGroup;
+  acceso: boolean | undefined;
 
-  constructor(private httpclient:HttpClient, public EmailService:EmailService, public CookieService: CookieService){
+  constructor(private httpclient:HttpClient, public EmailService:EmailService,
+    public CookieService: CookieService, public EmplogService: EmplogService ){
    
     this.datos= new FormGroup({
       correo: new FormControl([Validators.required, Validators.email]),
@@ -27,14 +32,38 @@ export class Forgot1Component implements OnInit {
   
   reset1(_form: NgForm){
     //this.CookieService.set('email', _form.value.email)
-    this.EmailService.envioCorreo({email: _form.value.email}).subscribe(res =>{ 
+    const cookieValue = this.CookieService.set("email", _form.value.email)
+    const cookieValue2 = this.CookieService.get("email")
+    this.EmplogService.isEmpleado(cookieValue2).subscribe(
+      res => {
+        console.log("---------")
+        console.log(res)
+        console.log("---------")  
+        if(res){
+          this.acceso = true
+          this.EmailService.envioCorreo({email: _form.value.email}).subscribe(res =>{ 
+            var u = JSON.stringify(res);
+            var e = JSON.parse(u)
+            console.log(e , " E parse mail")
+          })
+        } else {
+          console.log("nooo")
+          this.acceso = false
+        }
+      },
+      err => {
+        console.log(err)
+    }
+     )
 
-      var u = JSON.stringify(res);
-      var e = JSON.parse(u)
-      console.log(e , " E parse mail")
-    })
+    
+    
+    
 
-    //funcion  genera -> safdajsdvau121653712
+
+
+
+
     
   } 
 }
