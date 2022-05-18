1d244b0d51bb002dbdc7e242b8d00c0b7181f4f7
brunosaldana <brsase@fp.insjoaquimmir.cat>
2022-05-18 20:54:36 +0200

    aumento tamaño dist

--- frontendMcGym/src/environments/environment.prod.ts
+++ frontendMcGym/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  mapBoxToken: 'pk.eyJ1Ijoic2hhZG93c2F3YXkiLCJhIjoiY2t6eWZ5Zzd1MGJhMzJ2bGpjbW5qdjVocCJ9.QuvxMZregvVe0YYi3u68fQ'
+
 };
